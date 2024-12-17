dec9e26920533fe089b34a76d659960a07362b8f
xiaoheibaby <1321510204@qq.com>
2024-12-17 20:51:40 +0800

    init

---
+++ backend/src/main/java/com/xiaoheibaby/test/controller/RootController.java
@@ -0,0 +1,9 @@
+package com.xiaoheibaby.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+}
