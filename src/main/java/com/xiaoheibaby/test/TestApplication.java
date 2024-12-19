d4dbac0085fb9ecad6a2f528af3c9dc4dbd6f6c1
xiaoheibaby <1321510204@qq.com>
2024-12-19 20:25:51 +0800

    init

--- src/main/java/com/xiaoheibaby/test/TestApplication.java
+++ src/main/java/com/xiaoheibaby/test/TestApplication.java
@@ -1,13 +1,34 @@
 package com.xiaoheibaby.test;
 
-import org.springframework.boot.SpringApplication;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class TestApplication {
+public class TestApplication extends Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        // 创建 JavaFX 界面
+        Label label = new Label("Hello, JavaFX!");
+        Scene scene = new Scene(label, 300, 200);
 
+        primaryStage.setTitle("JavaFX Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        // 启动 Spring Boot 应用
+        new SpringApplicationBuilder(TestApplication.class)
+            .web(WebApplicationType.NONE) // 禁用 Web 环境
+            .run(args);
+
+        launch(args);
+    }
 }
