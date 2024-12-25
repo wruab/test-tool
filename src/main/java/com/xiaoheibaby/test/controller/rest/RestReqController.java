00e2d52881e34149889336867d46bc231f22f992
xiaoheibaby <1321510204@qq.com>
2024-12-25 19:34:00 +0800

    新增ip查询

--- src/main/java/com/xiaoheibaby/test/controller/rest/RestReqController.java
+++ src/main/java/com/xiaoheibaby/test/controller/rest/RestReqController.java
@@ -3,9 +3,7 @@ package com.xiaoheibaby.test.controller.rest;
 import com.xiaoheibaby.test.model.dto.IpInfoDTO;
 import com.xiaoheibaby.test.service.GeoLiteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/b")
 @RestController
