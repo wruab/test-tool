756b29bc3a7e105b8b5c419a4ab379925c4f1de7
xiaoheibaby <1321510204@qq.com>
2024-12-26 17:16:14 +0800

    更新

--- src/main/java/com/xiaoheibaby/test/model/dto/PixelData.java
+++ src/main/java/com/xiaoheibaby/test/model/dto/PixelData.java
@@ -1,8 +1,12 @@
 package com.xiaoheibaby.test.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PixelData {
     private int x;
     private int y;
